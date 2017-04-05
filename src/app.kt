213ffeefd841c09e9aabb2f97c3d7b05c932685e
su-kun1899 <higedrum.coz@gmail.com>
2017-04-05 21:04:48 +0900

    Add when statement sample

--- src/app.kt
+++ src/app.kt
@@ -43,4 +43,25 @@ fun main(args: Array<String>) {
     val score = 60
     val resultMessage = if (score > 70) "Great!" else "Don't mind."
     println(resultMessage)
+
+    // switchの代わりにwhen
+    var num = 3
+    when (num) {
+        0 -> println("Zero")
+        1 -> println("One")
+        2, 3 -> println("Two or Three")
+        in 4..10 -> println("Many")
+        else -> println("other")
+    }
+
+    // whenも値を返せる
+    num = 8
+    val whenResult = when (num) {
+        0 -> "Zero"
+        1 -> "One"
+        2, 3 -> "Two or Three"
+        in 4..10 -> "Many"
+        else -> "other"
+    }
+    println(whenResult)
 }
