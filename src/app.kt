3f4877dd2b46e923c32fc687f67b35681bda9278
su-kun1899 <higedrum.coz@gmail.com>
2017-04-05 20:20:47 +0900

    Use var and val

--- src/app.kt
+++ src/app.kt
@@ -1,6 +1,18 @@
 /**
+ * はじめてのことりん
+ *
  * Created by su-kun1899 on 2017/04/05.
  */
 fun main(args: Array<String>) {
     println("Hello, Kotlin!")
+
+    // val変数を使ってみる
+    val valMessage = "val Message!"
+    println(valMessage)
+
+    // var変数は再代入できる
+    var varMessage = "first var Message!"
+    println(varMessage)
+    varMessage = "second var Message"
+    println(varMessage)
 }
